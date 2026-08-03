f0c24a62856df61abb4ead7870ea9745f030e8ee
angel2379 <anglouison237@gmail.com>
2026-08-03 05:48:16 +0200

    Connexion Supabase

--- app/connexion/page.tsx
+++ app/connexion/page.tsx
@@ -1,4 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../../lib/supabase";
 export default function Connexion() {
+  const router = useRouter();
+
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+async function connexion() {
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  alert("Connexion réussie 🎉");
+  router.push("/");
+}
   return (
     <main className="min-h-screen bg-[#FFF8F7] flex items-center justify-center px-6">
       <div className="bg-white rounded-3xl shadow-xl p-8 w-full max-w-md">
@@ -11,16 +34,20 @@ export default function Connexion() {
         </p>
 
         <input
-          type="email"
-          placeholder="Adresse e-mail"
-          className="w-full border rounded-xl p-3 mb-4"
+           type="email"
+           placeholder="Adresse e-mail"
+           value={email}
+           onChange={(e) => setEmail(e.target.value)}
+           className="w-full border rounded-xl p-3 mb-4"
         />
 
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          className="w-full border rounded-xl p-3 mb-2"
-        />
+       <input
+         type="password"
+         placeholder="Mot de passe"
+         value={password}
+         onChange={(e) => setPassword(e.target.value)}
+         className="w-full border rounded-xl p-3 mb-2"
+      />
 
         <div className="text-right mb-6">
           <a href="#" className="text-pink-500 text-sm">
@@ -28,7 +55,10 @@ export default function Connexion() {
           </a>
         </div>
 
-        <button className="w-full bg-pink-600 hover:bg-pink-700 text-white rounded-xl py-3 font-bold transition">
+        <button
+         onClick={connexion}
+         className="w-full bg-pink-600 hover:bg-pink-700 text-white rounded-xl py-3 font-bold transition"
+       >
           Se connecter
         </button>
       </div>
